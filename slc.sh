0c4b7ab4032c72cf685108c68dcb4d4d9af68e5c
David <david@gmail.com>
2022-05-17 23:23:48 -0400

    lik

---
+++ slc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo single line comment starts
+# this is just a single line comment
+# whatever you put here wont execute
+# its just a comment
+echo single line cooment ends
+date 
+echo thanks for coming to my ted talk
